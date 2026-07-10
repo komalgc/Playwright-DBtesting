187b18a9c7aa25f25f237d92f323c3826881c297
komalgc <komalgc17@gmail.com>
2026-07-10 11:02:58 -0600

    config data

--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -1,4 +1,5 @@
 import {test, expect} from '@playwright/test'
+import { SauceDemoUsers } from '../utils/test-data';
 
 test('sauce demo login', async ({page}) => {
 
@@ -9,3 +10,22 @@ test('sauce demo login', async ({page}) => {
     await expect(page.getByText('Products')).toBeVisible();
 
 })
+
+test('sauce demo login with environment credential', async ({page}) => {
+
+    await page.goto('/')
+    await page.getByPlaceholder('Username').fill(process.env.SAUCEDEMO_STANDARD_USER!);
+    await page.getByPlaceholder('Password').fill(process.env.SAUCEDEMO_PASSWORD!);
+    await page.getByRole('button', {name : 'Login'}).click();
+    await expect(page).toHaveURL('/inventory.html');
+    await expect(page.getByText('Products')).toBeVisible();
+
+})
+
+test('login with test data helper', async ({ page }) => {
+await page.goto('https://www.saucedemo.com/');
+await page.locator('#user-name').fill(SauceDemoUsers.standard.username);
+await page.locator('#password').fill(SauceDemoUsers.standard.password);
+await page.locator('#login-button').click();
+await expect(page).toHaveURL('/inventory.html');
+});
